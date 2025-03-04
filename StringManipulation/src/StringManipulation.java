344f2a8e6111e994b2c8a5ca54961f8d4fea562b
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-03 19:34:29 -0500

    added

---
+++ StringManipulation/src/StringManipulation.java
@@ -0,0 +1,41 @@
+// Importing the necessary Java package for handling user input
+import java.util.Scanner;
+
+public class StringManipulation
+    {
+        public static void main(String[] args)
+        {
+                // Create a scanner object to take user input
+                Scanner scanner = new Scanner(System.in);
+
+                // Prompt the user to enter a full sentence
+                System.out.println("Enter a full sentence:");
+                String sentence = scanner.nextLine();
+
+                // Print the length of the sentence
+                //TODO
+
+                // Print the first and last character of the sentence
+                //TODO
+
+                // Ask for another string to compare
+                //TODO
+
+                // Compare the two sentences using equals and equalsIgnoreCase
+                //TODO
+
+                // Lexicographical comparison using compareTo
+                //TODO
+
+                // Demonstrate substring and replace methods
+                //TODO
+
+                // Convert sentence to upper and lower case
+                //TODO
+
+                // Formatting with strings and numbers
+                //TODO
+        }
+    }
+
+
