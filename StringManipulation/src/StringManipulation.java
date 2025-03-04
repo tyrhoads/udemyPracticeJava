05858a9f74b179e72ec8904e51860caf0d338fcb
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-03 23:04:35 -0500

    half way through it

--- StringManipulation/src/StringManipulation.java
+++ StringManipulation/src/StringManipulation.java
@@ -14,12 +14,20 @@ public class StringManipulation
 
                 // Print the length of the sentence
                 //TODO
-
+                int length = sentence.length();
+                System.out.println("The length of the sentence is: " + length);
                 // Print the first and last character of the sentence
                 //TODO
+                char first = sentence.charAt(0);
+                char last = sentence.charAt(length-1);
+                System.out.println("The first character of the sentence is: " + first);
+                System.out.println("The last character of the sentence is: " + last);
 
                 // Ask for another string to compare
                 //TODO
+                System.out.println("Please enter a new sentence to compare too: ");
+                String secondSentence = scanner.nextLine();
+
 
                 // Compare the two sentences using equals and equalsIgnoreCase
                 //TODO
