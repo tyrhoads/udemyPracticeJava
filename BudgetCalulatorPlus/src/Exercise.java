7d00c658c51d3bff0c8d6ac1994eac58965746c2
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-05 22:44:32 -0500

    done

--- BudgetCalulatorPlus/src/Exercise.java
+++ BudgetCalulatorPlus/src/Exercise.java
@@ -1,33 +1,56 @@
 // Importing the necessary Java package for handling user input
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Exercise {
     public static void budgetTracker() {
         // Create a scanner object to take user input
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
         // Prompt the user to enter their monthly income
         System.out.println("Enter your monthly income:");
-        double income = scanner.nextDouble();
+        double income = scan.nextDouble();
+        scan.nextLine();
 
         // Initialize variables for expenses
-        //TODO
+        ArrayList<Double> expenses = new ArrayList<Double>();
 
         // Prompt the user to enter the number of expenses they want to track
         // Remember to consume the newline left-over
-        //TODO
+        System.out.println("Enter the number of expenses: ");
+        int numberOfExpenses = scan.nextInt();
 
         // Loop to track each expense
-        //TODO
+        int counter = 0;
+        while (counter < numberOfExpenses)
+        {
+            System.out.printf("Enter expense %s:\n",counter+1);
+            double expenseAmount = scan.nextDouble();
+            expenses.add(expenseAmount);
+            counter++;
+        }
 
         // Calculate the remaining balance
         //TODO
+        double totalExpense = 0.0;
+        counter =0;
+        while (counter < numberOfExpenses)
+        {
+            totalExpense += expenses.get(counter);
+            counter ++;
+        }
 
         // Display the total income, total expenses, and remaining balance
-        //TODO
+        System.out.printf("Total Income: $%.2f\n",income);
+        System.out.printf("Total Expenses: $%.2f\n",totalExpense);
+        double remainingBalance = income - totalExpense;
+        System.out.printf("Remaining Balance: $%.2f\n",remainingBalance);
 
         // Provide a financial warning if expenses exceed income
-        //TODO
+        if(income<totalExpense)
+        {
+            System.out.println("Get your shit together.");
+        }
     }
     public static void main(String[] args) {
         Exercise.budgetTracker();
