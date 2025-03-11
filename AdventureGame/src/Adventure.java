00fa4d967170436e449c005d53efafb8a1c4e1cd
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-10 22:32:39 -0400

    finally got it to start runnign the basics

--- AdventureGame/src/Adventure.java
+++ AdventureGame/src/Adventure.java
@@ -1,21 +1,24 @@
 import java.util.Scanner;
-    public class Adventure
-{
-    // Enum to represent different actions in the adventure game
-    enum Direction {
-        NORTH, SOUTH, EAST, WEST, QUIT
-    }
 
-    public void startAdventure()
-    {
+
+// Enum to represent different actions in the adventure game
+enum Direction {
+    NORTH, SOUTH, EAST, WEST, QUIT
+}
+public class Adventure {
+    public static void startAdventure() {
         // Create a scanner object to take user input
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         Direction direction;
 
         // Introduction to the game
         System.out.println("Welcome to the Ultimate Adventure Game!");
         System.out.println("You find yourself in a dark forest. Choose a direction to explore.");
-
+        System.out.println("North , South , East or West....or Quit to exit");
+        String userInput = scan.nextLine();
+        while(userInput != "QUIT"){
+            System.out.println();
+        }
         // Game loop: keep asking the player to choose a direction until they quit
         // Prompt the user to enter a direction
         // Use a switch statement to handle the player's choice
@@ -24,8 +27,20 @@ import java.util.Scanner;
 
 
     }
-    public static void main(String[] args){
 
+    public static void main(String[] args) {
+        Adventure.startAdventure();
     }
+
 }
+
+
+
+
+
+
+
+
+
+
 
