ad23fcf58b2466c70dca6b349ccbf1972f1035a5
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-10 23:16:24 -0400

    got the loop to run and catch the error

--- AdventureGame/src/Adventure.java
+++ AdventureGame/src/Adventure.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 // Enum to represent different actions in the adventure game
 enum Direction {
-    NORTH, SOUTH, EAST, WEST, QUIT
+    NORTH, SOUTH, EAST, WEST
 }
 public class Adventure {
     public static void startAdventure() {
@@ -15,16 +15,46 @@ public class Adventure {
         System.out.println("Welcome to the Ultimate Adventure Game!");
         System.out.println("You find yourself in a dark forest. Choose a direction to explore.");
         System.out.println("North , South , East or West....or Quit to exit");
-        String userInput = scan.nextLine();
-        while(userInput != "QUIT"){
-            System.out.println();
+        String userInput = "tester";
+        while(!userInput.equalsIgnoreCase("QUIT"))
+        {
+
+            userInput = scan.nextLine().toUpperCase();
+            try {
+                direction = Direction.valueOf(userInput);
+            }
+            catch (IllegalArgumentException e)
+            {
+                System.out.println("Please pick a valid entry");
+                continue;
+            }
+            switch (direction)
+            {
+                case NORTH:
+                    System.out.println("You went North and saw a Woman bathing nude in the river");
+                    break;
+                case SOUTH:
+                    System.out.println("You went North and saw a Woman bathing nude in the river");
+                    break;
+                case EAST:
+                    System.out.println("You went North and saw a Woman bathing nude in the river");
+                    break;
+                case WEST:
+                    System.out.println("You went North and saw a Woman bathing nude in the river");
+                    break;
+
+            }
+            System.out.println("North , South , East or West....or Quit to exit");
+            userInput = scan.nextLine().toUpperCase();
+
+
         }
         // Game loop: keep asking the player to choose a direction until they quit
         // Prompt the user to enter a direction
         // Use a switch statement to handle the player's choice
         // Ask again if the input is invalid
         //TODO
-
+    scan.close();
 
     }
 
