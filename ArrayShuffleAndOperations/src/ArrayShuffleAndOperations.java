7db4d31a5f2c6268673103e764629b044ba523a5
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-12 22:52:02 -0400

    added

---
+++ ArrayShuffleAndOperations/src/ArrayShuffleAndOperations.java
@@ -0,0 +1,49 @@
+// Importing necessary Java package for utility functions
+import java.util.Arrays;
+import java.util.Random;
+
+public class ArrayShuffleAndOperations
+{
+    public static void main(String[] args)
+    {
+
+    }
+    public void arrayShuffleAndOperations() {
+        // Declare and initialize an array with 10 integers
+        int[] numbers = {12, 43, 56, 78, 34, 23, 89, 90, 65, 31};
+
+        // Display the original array
+        // Shuffle the array
+        // Find and display the sum, minimum, and maximum of the array
+        // Sort the array and display the sorted array
+        //TODO
+
+    }
+
+    // Method to shuffle the array
+    public void shuffleArray(int[] array) {
+        Random random = new Random();
+        for (int i = array.length - 1; i > 0; i--) {
+            // Swap the elements
+            //TODO
+        }
+    }
+
+    // Method to calculate the sum of the array
+    public static int calculateSum(int[] array) {
+        //TODO
+        return 0;
+    }
+
+    // Method to find the minimum value in the array
+    public int findMin(int[] array) {
+        //TODO
+        return 0;
+    }
+
+    // Method to find the maximum value in the array
+    public int findMax(int[] array) {
+        //TODO
+        return 0;
+    }
+}
