7f592b8fcc1c9f0b2dde0365277813cde668ce5e
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-13 10:14:14 -0400

    added a function to clean up code

--- Fibonacci/src/FibonacciCalulator.java
+++ Fibonacci/src/FibonacciCalulator.java
@@ -8,31 +8,23 @@ public class FibonacciCalulator
         System.out.println("Please enter a number for  Fibonacci sequence");
         int userInput = scan.nextInt();
         scan.nextLine();
-        double firstNumber = 0;
-        double secondNumber = 1;
-        if(userInput == 1)
-        {
-            System.out.print(firstNumber);
-        }
-        else if (userInput == 2)
-        {
-            System.out.print(firstNumber + "," + secondNumber);
-        }
-        else
-        {
-            System.out.print(firstNumber + "," + secondNumber);
-            double nextNumber;
 
-            for(int i = 2; i < userInput; i++)
+
+
+
+            for(int i = 0; i < userInput; i++)
             {
-                nextNumber = firstNumber + secondNumber;
-
-                System.out.print( "," + nextNumber);
-                firstNumber = secondNumber;
-                secondNumber = nextNumber;
+                System.out.print(fibonacci(i) +  "," );
 
             }
-        }
 
+
+    }
+
+    public static int fibonacci (int n){
+        if(n<=1){
+            return n;
+        }
+        return fibonacci(n-1)+fibonacci(n-2);
     }
 }
