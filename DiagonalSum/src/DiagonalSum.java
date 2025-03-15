9f6cf5f0b26aabe685668f0ed979f5a65eb415ab
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-15 00:22:00 -0400

    made methods for the steps so far

--- DiagonalSum/src/DiagonalSum.java
+++ DiagonalSum/src/DiagonalSum.java
@@ -5,22 +5,37 @@ public class DiagonalSum
 {
     public static void main(String[] args)
     {
-        Scanner scan = new Scanner(System.in);
+         Scanner scan = new Scanner(System.in);
+        int n = getNumber(scan);
+        int[][] matrix = new int[n][n];
+        fillMatrix(matrix,n,scan);
+        System.out.println(Arrays.deepToString(matrix));
+        scan.close();
+
+    }
+
+    public static int getNumber(Scanner scan)
+    {
+
         System.out.println("Please enter a number for the size of the matrix..");
         int matrixSize = scan.nextInt();
         scan.nextLine();
-        int[][] matrix = new int[matrixSize][matrixSize];
-        for (int i = 0; i<matrixSize; i++)
+
+
+        return matrixSize;
+    }
+    public static void fillMatrix(int[][] array, int n, Scanner scan)
+    {
+
+        for (int i = 0; i<n; i++)
         {
-            for (int j = 0; j<matrixSize; j++)
+            for (int j = 0; j<n; j++)
             {
                 System.out.println("Please enter next number to fill the matrix..");
-                matrix[i][j]= scan.nextInt();
+                array[i][j]= scan.nextInt();
                 scan.nextLine();
             }
         }
-        System.out.println(Arrays.deepToString(matrix));
 
-        scan.close();
     }
 }
