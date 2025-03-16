11673f1d1e222a9da67aeeb056498163ebe2e011
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-16 18:25:21 -0400

    finished everything works like instructed

--- SpaceTravelSimulatorPlus/src/SpaceTravelPlus.java
+++ SpaceTravelSimulatorPlus/src/SpaceTravelPlus.java
@@ -10,26 +10,17 @@ public class SpaceTravelPlus
         // Declare and initialize a 2D array representing sectors in space
         String[][] spaceGrid = {
                 {"Empty", "Asteroids", "Planet"},
-                {"Alien Ship", "Empty", "Space Station"},
+                {"Alien Ship", "Zeon warrior pincses", "Space Station"},
                 {"Comet", "Black Hole", "Empty"}
         };
 
         Random random = new Random();
         System.out.println("Space Grid:");
-        for (int i = 0; i<spaceGrid.length; i++)
-             {
-                 for (int j = 0; j <spaceGrid.length; j++)
-                 {
-                    System.out.print(spaceGrid[i][j] + " ");
-                 }
-                System.out.println(" ");
-            }
-        // Display the initial state of the space grid
-        //TODO
+        displayGrid(spaceGrid);
+
         System.out.println(" ");
         System.out.println("We are ");
         // Simulate traveling through space by visiting random sectors
-        //TODO
 
         int totalEvents = 5;
         for (int i = 0; i < totalEvents; i++) {
@@ -40,6 +31,10 @@ public class SpaceTravelPlus
             System.out.println("Visiting sector [" + row + "][" + col + "]: " + spaceGrid[row][col]);
         }
 
+        System.out.println("Total empty sectors: " + countEmptySectors(spaceGrid));
+        System.out.println("Largest event in space grid: " + findLargestEvent(spaceGrid));
+
+
         // Calculate the number of "Empty" sectors
         //TODO
 
@@ -48,19 +43,50 @@ public class SpaceTravelPlus
     }
 
     // Method to display the space grid
-    public void displayGrid(String[][] grid) {
-        //TODO
+    public static void displayGrid(String[][] grid) {
+        for (int i = 0; i<grid.length; i++)
+        {
+            for (int j = 0; j <grid.length; j++)
+            {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
     }
 
     // Method to count the number of "Empty" sectors in the space grid
-    public int countEmptySectors(String[][] grid)
+    public static int countEmptySectors(String[][] grid)
     {
-        return 0;
+        int emptyTotal = 0;
+        for (int i = 0; i<grid.length; i++)
+        {
+            for (int j = 0; j < grid.length; j++)
+            {
+                if(grid[i][j].equals("Empty"))
+                {
+                    emptyTotal++;
+                }
+                System.out.print(grid[i][j] + " ");
+            }
+        }
+        return emptyTotal;
     }
 
     // Method to find the largest event in the space grid (lexicographically)
-    public String findLargestEvent(String[][] grid)
+    public static String findLargestEvent(String[][] grid)
     {
-        return "sd";
+        String largestEvent = grid[0][0];
+        for (int i = 0; i<grid.length; i++)
+        {
+            for (int j = 0; j < grid.length; j++)
+            {
+                if(largestEvent.compareTo(grid[i][j]) < 0 )
+                {
+                    largestEvent = grid[i][j];
+                }
+
+            }
+        }
+        return largestEvent;
     }
 }
