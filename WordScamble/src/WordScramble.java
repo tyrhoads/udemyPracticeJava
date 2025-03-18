5fa986d64fcbb88ce223a5687e021474b2fbcd3f
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-17 20:35:55 -0400

    set up getword method

--- WordScamble/src/WordScramble.java
+++ WordScamble/src/WordScramble.java
@@ -1,3 +1,17 @@
-public class WordScramble {
+import java.util.Random;
 
+public class WordScramble {
+    public static void main(String[] args) {
+        String something = getWord();
+        System.out.println(something);
+
+    }
+    public static String getWord()
+    {
+        String[] words = {"delightful","intelligent","conquer","transform","persuade",
+                          "advance","inspiration","serenity","beauty","harmony"};
+        Random random = new Random();
+        int index = random.nextInt(10);
+        return words[index];
+    }
 }
