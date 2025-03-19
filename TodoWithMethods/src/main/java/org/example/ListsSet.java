bbf7e76092213daff97ee10fe0cb5cbc10924ffb
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-18 22:11:55 -0400

    added

---
+++ TodoWithMethods/src/main/java/org/example/ListsSet.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ListsSet {
+    // Declare a global ArrayList to store the TODO list
+    private ArrayList<String> todoList = new ArrayList<>();
+
+    public void manageTodoList() {
+        // Create a scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        System.out.println("Welcome to the TODO List Application!");
+
+        // Menu loop
+        // Display the menu options
+        // Consume the newline character
+        // Handle the menu choice
+        //TODO
+    }
+
+    // Method to display the menu
+    private void displayMenu() {
+        //TODO
+    }
+
+    // Method to add a task to the TODO list
+    private void addTask(Scanner scanner) {
+        //TODO
+    }
+
+    // Method to remove a task from the TODO list
+    private void removeTask(Scanner scanner) {
+        // Display the current tasks
+        // Remove the task
+        //TODO
+    }
+
+    // Method to display all tasks in the TODO list
+    private void displayTasks() {
+        //TODO
+    }
+}
+
+
