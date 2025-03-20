3d59f7318ab4fa01be02c4cca4555597dc5b2f0c
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-19 23:15:15 -0400

    added

---
+++ RemovingDuplicatArray/src/RemoveArray.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class RemoveArray
+{
+    public static void main(String[] args)
+    {
+        int[] myarray = {1,3,3,4,6,21,6,3,4,6,8,9,4,2,4,564,1,3,5,6,7,9,4,2,};
+    }
+    public static int[] uniqueArray(int[] array)
+    {
+        int[] temp = new int[array.length];
+        int j = 0;
+
+        for (int i = 0; i < array.length-1 ;i++)
+        {
+
+
+        }
+
+        return temp;
+    }
+}
