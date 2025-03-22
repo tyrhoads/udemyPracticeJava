f93793bc5df56f2c51f0294dcfeb53da3e17179c
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-22 16:04:00 -0400

    added

---
+++ MathQuiz/src/MathQuizGame.java
@@ -0,0 +1,8 @@
+public class MathQuizGame
+{
+    // Main method to start the quiz game
+    public static void main(String[] args) {
+        MathQuiz quiz = new MathQuiz();
+        quiz.startQuiz();
+    }
+}
