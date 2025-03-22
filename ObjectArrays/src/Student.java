a06f32209e97ee8dfaadfc11d825a1f3539d808f
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-22 15:42:14 -0400

    added

---
+++ ObjectArrays/src/Student.java
@@ -0,0 +1,13 @@
+public class Student
+{
+    String name;
+    int rollNumber;
+    int age;
+    double height;
+
+    // Constructor to initialize Student object
+    public Student(String name, int rollNumber, int age, double height)
+    {
+        // COMPLETE THIS PART
+    }
+}
