ce3cf0f7b7a4aa3799aba5a151fea0f4928f3d3e
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-23 15:40:26 -0400

    finished'

--- WordGuessingGame/src/AdvancedGame.java
+++ WordGuessingGame/src/AdvancedGame.java
@@ -9,4 +9,26 @@ class AdvancedGame extends InputGame
         super(wordToGuess);
         this.random = new Random();
     }
+
+    @Override
+    public void displayHint()
+    {
+        //super.displayHint();
+        if (attempts>0)
+        {
+            reveledLetters();
+            System.out.println("🔍 Revealed so far: " + getRevealedWord());
+        }
+    }
+    private void reveledLetters()
+    {
+        int index;
+        do
+        {
+            index = random.nextInt(wordToGuess.length());
+
+        }
+        while (reveledLetters[index] != '*');
+        reveledLetters[index] = wordToGuess.charAt(index);
+    }
 }
