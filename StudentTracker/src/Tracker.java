323c959175330cc3db09324f77aab634c10556ba
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-22 21:16:05 -0400

    added

---
+++ StudentTracker/src/Tracker.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Tracker {
+
+
+
+    public void gradesTrackerApp() {
+        // Initialize students array
+        Student[] students = new Student[3];
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Student Grades Tracker!");
+
+        // Loop to create and populate student objects
+        for (int i = 0; i < students.length; i++) {
+            // Consume newline character
+            // Create a new Student object and add it to the array
+            //TODO
+        }
+
+        // Display student details
+        //TODO
+    }
+}
+
+
+}
