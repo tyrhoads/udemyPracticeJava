9097c8a423dff0565dc1b0b51b82b07061f8d70a
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-22 21:16:24 -0400

    added

---
+++ StudentTracker/src/Student.java
@@ -0,0 +1,37 @@
+// Class to represent a Student
+class Student
+{
+    private String name;
+    private double grade;
+
+
+    // Parameterized constructor
+    public Student(String name, double grade)
+    {
+        this.name= name;
+        this.grade =grade;
+    }
+
+    // Method to get student details
+    public String getDetails()
+    {
+        //TODO
+    }
+
+    // Setters and getters (optional for this exercise)
+    public void setName(String name)
+    {
+        //TODO
+    }
+
+    public void setGrade(double grade) {
+        //TODO
+    }
+
+    public String getName() {
+        return //TODO;
+    }
+
+    public double getGrade() {
+        return //TODO;
+    }
