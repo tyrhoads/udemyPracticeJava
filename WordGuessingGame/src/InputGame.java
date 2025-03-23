7fa901716840c9aa775fd97ca6c9e1cdde3cc3f6
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-23 15:40:06 -0400

    finsihed

--- WordGuessingGame/src/InputGame.java
+++ WordGuessingGame/src/InputGame.java
@@ -8,4 +8,27 @@ class InputGame extends Game
         super(wordToGuess);
         this.scan = new Scanner(System.in);
     }
+
+    @Override
+    public void gamePlay()
+    {
+        super.gamePlay();
+        super.displayHint();
+        String userInput = "";
+        while (!userInput.equalsIgnoreCase(wordToGuess)) {
+            System.out.println("Please enter your guess for the word ");
+            displayHint();
+            userInput = scan.nextLine();
+            attempts++;
+            if (userInput.equalsIgnoreCase(wordToGuess)) {
+                System.out.println("🎉 Congratulations! You've guessed the word in " + attempts + " attempts.");
+                return;
+            } else {
+                System.out.println("❌ Incorrect guess. Try again!");
+            }
+            System.out.println("Revealed so far: " + getRevealedWord());
+        }
+
+    }
+
 }
