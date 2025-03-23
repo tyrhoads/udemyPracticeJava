c310f0750ab07cbb9216a0cfc4aa3892f3acb178
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-22 21:51:54 -0400

    finished

--- DiceGame/src/LuckyDiceAdventure.java
+++ DiceGame/src/LuckyDiceAdventure.java
@@ -1,5 +1,9 @@
-ublic class LuckyDiceAdventure {
-    public static void main(String[] args) {
+import java.util.Scanner;
+
+public class LuckyDiceAdventure
+{
+    public static void main(String[] args)
+    {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to Lucky Dice Adventure!");
@@ -9,22 +13,29 @@ ublic class LuckyDiceAdventure {
         Game game = new Game(targetScore);
 
         boolean playing = true;
-        while (playing) {
+        while (playing)
+        {
             System.out.println("Press 'r' to roll the dice, 's' to stop:");
             char choice = scanner.next().charAt(0);
 
-            if (choice == 'r') {
+            if (choice == 'r')
+            {
                 game.rollDice();
                 game.displayScore();
-                if (game.isTargetReached()) {
+                if (game.isTargetReached())
+                {
                     System.out.println("Congratulations! You've reached the target score!");
                     playing = false;
                 }
-            } else if (choice == 's') {
+            }
+            else if (choice == 's')
+            {
                 System.out.println("You chose to stop. Final Score: ");
                 game.displayScore();
                 playing = false;
-            } else {
+            }
+            else
+            {
                 System.out.println("Invalid choice. Please try again.");
             }
         }
