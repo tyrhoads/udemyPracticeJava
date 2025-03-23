24b349cb8203993d30ed0db1880feabe34be0df9
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-22 21:47:50 -0400

    added

---
+++ DiceGame/src/Dice.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class Dice
+{
+    private int value;
+
+    // Step 1: Default Constructor to initialize dice
+    public Dice()
+    {
+        // COMPLETE THIS PART
+    }
+
+    // Step 2: Method to roll the dice
+    public void roll()
+    {
+        Random rand = new Random();
+        // COMPLETE THIS PART
+    }
+
+    // Step 3: Method to get the current value of the dice
+    public int getValue()
+    {
+        return 1; // COMPLETE THIS PART;
+    }
+}
