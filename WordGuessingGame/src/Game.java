40a9e2bac3b262cd9d89993576740ccac601a787
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-23 14:41:02 -0400

    setUp

--- WordGuessingGame/src/Game.java
+++ WordGuessingGame/src/Game.java
@@ -1,4 +1,28 @@
+import java.util.Scanner;
+import java.util.Random;
+
 class Game
 {
+    protected String wordToGuess;
+    protected int attempts;
+    protected char[] reveledLetters;
+
+    public Game(String wordToGuess)
+        {
+            this.wordToGuess = wordToGuess;
+            this.attempts = 0;
+            this.reveledLetters= new char[wordToGuess.length()];
+            for (int i=0; i<wordToGuess.length(); i++)
+            {
+                reveledLetters[i] = '-';
+            }
+        }
+
+    public void gamePlay()
+    {
+        System.out.println("Welcome To the word guessing Game:");
+        System.out.println("Time to try and guess the word:");
+
+    }
 
 }
