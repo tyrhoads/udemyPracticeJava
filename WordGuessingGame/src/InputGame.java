a02e9e31d0b4a8575f11987f50d267961665ede5
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-23 14:41:14 -0400

    setUp

--- WordGuessingGame/src/InputGame.java
+++ WordGuessingGame/src/InputGame.java
@@ -1,4 +1,11 @@
+import java.util.Scanner;
+
 class InputGame extends Game
 {
+    Scanner scan;
 
+    public InputGame(String wordToGuess) {
+        super(wordToGuess);
+        this.scan = new Scanner(System.in);
+    }
 }
