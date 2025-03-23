b46576e8a27650efabb1234eaa9bfac04afe99ad
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-23 15:47:00 -0400

    setUP

--- AnimalExercies/src/Dog.java
+++ AnimalExercies/src/Dog.java
@@ -1,4 +1,18 @@
  class Dog extends Animal
  {
+     private String breed;
+
+     // Constructor using super to call the parent class constructor
+     // Call the parent class constructor
+     //TODO
+
+     // Getter for breed
+     //TODO
+
+     // Method to display details using parent and child attributes
+     public void displayDetails() {
+         System.out.println("Dog's Name: " + getName());
+         System.out.println("Dog's Breed: " + getBreed());
+     }
 
 }
