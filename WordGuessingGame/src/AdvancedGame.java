f5c1d7fdc9308ff864ab76edc4c67849d8cc5016
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-23 14:41:28 -0400

    setUp

--- WordGuessingGame/src/AdvancedGame.java
+++ WordGuessingGame/src/AdvancedGame.java
@@ -1,4 +1,12 @@
+import java.util.Random;
+
 class AdvancedGame extends InputGame
 {
+    Random random;
 
+
+    public AdvancedGame(String wordToGuess) {
+        super(wordToGuess);
+        this.random = new Random();
+    }
 }
