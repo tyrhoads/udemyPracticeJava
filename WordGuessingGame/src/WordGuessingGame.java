b526e3197eb24d36229419b3ed5d8b20fe872e45
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-23 15:38:20 -0400

    finished

--- WordGuessingGame/src/WordGuessingGame.java
+++ WordGuessingGame/src/WordGuessingGame.java
@@ -1,7 +1,13 @@
+import java.util.Random;
+
 public class WordGuessingGame
 {
     public static void main(String[] args)
     {
         String[] wordToGuess = {"Tyler","Fuck","Bitch","Butt","Boobs", "Shit"};
+        Random random = new Random();
+        int wordIndex = random.nextInt(wordToGuess.length);
+        AdvancedGame game = new AdvancedGame(wordToGuess[wordIndex]);
+        game.gamePlay();
     }
 }
