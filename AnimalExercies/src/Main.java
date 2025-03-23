7d19a3ec0635b8c6ea3092773b1fae7dad941c62
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-23 15:46:34 -0400

    setUP:

--- AnimalExercies/src/Main.java
+++ AnimalExercies/src/Main.java
@@ -2,6 +2,9 @@ public class Main
 {
     public static void main(String[] args)
     {
-
+        // Create an object of the Dog class
+        Dog dog = new Dog("Buddy", "Golden Retriever");
+        // Display dog details
+        dog.displayDetails();
     }
 }
