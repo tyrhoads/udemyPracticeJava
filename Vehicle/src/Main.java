2be9afe4084bc14f6e4d1daa47723a08e1e63825
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-24 22:48:44 -0400

    finsihed

--- Vehicle/src/Main.java
+++ Vehicle/src/Main.java
@@ -2,6 +2,9 @@ public class Main
 {
     public static void main(String[] args)
     {
-
+        Vehicle car = new Car();
+        Vehicle bike = new Bike();
+        car.drive();
+        bike.drive();
     }
 }
