a9af52e7336a102152190f21e829c6303803bb9f
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-24 22:49:10 -0400

    finsiehd

--- Vehicle/src/Car.java
+++ Vehicle/src/Car.java
@@ -1,3 +1,10 @@
 public class Car extends Vehicle
 {
+    @Override
+    void drive()
+    {
+        System.out.println("The Car is Driving");
+
+    }
+    // TODO: Override the drive method for Car
 }
