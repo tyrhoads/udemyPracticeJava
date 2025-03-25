b10b382e8358afb8b367cda104cc4d045d767bc0
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-24 22:49:24 -0400

    finsihed

--- Vehicle/src/Bike.java
+++ Vehicle/src/Bike.java
@@ -1,4 +1,9 @@
 public class Bike extends Vehicle
 {
-
+    @Override
+    void drive()
+    {
+        System.out.println("The Bike is Riding");
+    }
+    // TODO: Override the drive method for Bike
 }
