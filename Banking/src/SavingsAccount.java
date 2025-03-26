238a2880609c3200177fe6897e62b4459a19d15a
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-25 22:51:19 -0400

    almost done

--- Banking/src/SavingsAccount.java
+++ Banking/src/SavingsAccount.java
@@ -1,2 +1,20 @@
-public class SavingsAccount extends BankAccount{
+public class SavingsAccount extends BankAccount
+{
+
+    public SavingsAccount(String accountNumber, double balance)
+    {
+        super(accountNumber,balance);
+    }
+    @Override
+    public void withdraw(double amount)
+    {
+        double currentBalance = getBalance();
+        if(currentBalance>amount)
+        {
+            System.out.println("Withdrawing " + amount+ " from your account.");
+
+        }
+    }
+
+
 }
