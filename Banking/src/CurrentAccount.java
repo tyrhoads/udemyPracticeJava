d4d43d244386ccae1ee95965b9dc3532d337bf0d
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-25 22:58:40 -0400

    finished

--- Banking/src/CurrentAccount.java
+++ Banking/src/CurrentAccount.java
@@ -1,2 +1,26 @@
-public class CurrentAccount extends BankAccount {
+public class CurrentAccount extends BankAccount
+{
+    private final double overDraftLimits;
+
+    public CurrentAccount(String accountNumber, double balance, double overDraftLimits)
+    {
+        super(accountNumber, balance);
+        this.overDraftLimits = overDraftLimits;
+    }
+
+    @Override
+    public void withdraw(double amount)
+    {
+        double currentBalance = getBalance();
+        if(currentBalance + overDraftLimits > amount)
+        {
+            System.out.println("Withdrawing " + amount+ " from your account.");
+            deposit(-amount);
+
+        }
+        else
+        {
+            System.out.println("Exceeds overdraft limit.");
+        }
+    }
 }
