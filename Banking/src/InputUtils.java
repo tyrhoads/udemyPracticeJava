55a9ad35b24a16389eb35dc2c0592bc357d9f592
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-25 23:08:12 -0400

    finished

--- Banking/src/InputUtils.java
+++ Banking/src/InputUtils.java
@@ -1,3 +1,38 @@
+import java.util.Scanner;
+
 public class InputUtils
 {
+    Scanner scanner;
+
+    public InputUtils(Scanner scanner)
+    {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public double readDouble()
+    {
+        while (!scanner.hasNextDouble())
+        {
+            System.out.println("Please enter a valid number.");
+            scanner.next();
+        }
+        double value = scanner.nextDouble();
+        scanner.nextLine();
+        return value;
+    }
+    public String readString()
+    {
+        return scanner.nextLine();
+    }
+    public int readInt()
+    {
+        while (!scanner.hasNextInt())
+        {
+            System.out.println("Please enter a valid number.");
+            scanner.next();
+        }
+        int value = scanner.nextInt();
+        scanner.nextLine();
+        return value;
+    }
 }
