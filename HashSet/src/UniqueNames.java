6b962924afdccc775892578ab537fd71a8923cd2
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-27 22:18:02 -0400

    added

---
+++ HashSet/src/UniqueNames.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+public class UniqueNames<S> {
+
+    public static void main(String[] args)
+    {
+        HashSet<String> set = new HashSet<>();
+
+        // Add names to the HashSet and include a Duplicate entry
+        //TODO
+
+        // Display the names in the HashSet
+        System.out.println("Unique Names:");
+        //TODO
+
+    }
+
+}
