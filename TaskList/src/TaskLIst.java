f99108f5cd35aaae3e74f66a1fe59de52d41922b
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-27 23:15:41 -0400

    finished

--- TaskList/src/TaskLIst.java
+++ TaskList/src/TaskLIst.java
@@ -1,2 +1,22 @@
-public class TaskLIst {
+import java.util.ArrayDeque;
+
+public class TaskLIst
+{
+    public static void main (String[] args)
+    {
+        // Create an ArrayDeque to represent a task queue
+        //TODO
+        ArrayDeque<String> task = new ArrayDeque<>();
+        // Add tasks to the queue
+        //TODO
+        task.add("Processing: Task 1: Clean the house");
+        task.add("Processing: Task 2: Complete Java exercise");
+        task.add("Processing: Task 3: Pay utility bills");
+
+        // Process and display each task in FIFO order
+        System.out.println("Processing tasks:");
+        //TODO
+        while (!task.isEmpty())
+            System.out.println(task.poll());
+    }
 }
