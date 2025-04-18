a55b14d6f5f7d1569f02c7a964a7536b2a16164c
Tyler Rhoads <tyrhoads@gmail.com>
2025-04-17 22:48:20 -0400

    added style sheet

--- UiFX/src/main/java/com/tutorialseu/fxmldemo/uifx/HelloApplication.java
+++ UiFX/src/main/java/com/tutorialseu/fxmldemo/uifx/HelloApplication.java
@@ -6,13 +6,17 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("complex_ui.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/com/tutorialseu/fxmldemo/uifx/style.css")).toExternalForm());
+
+        ;stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
