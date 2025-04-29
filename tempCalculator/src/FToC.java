05e016d95b20c1d67cd57538910ade195d2e851e
Tyler Rhoads <tyrhoads@gmail.com>
2025-04-28 22:03:17 -0400

    updated

--- tempCalculator/src/FToC.java
+++ tempCalculator/src/FToC.java
@@ -4,7 +4,40 @@ public class FToC
 {
     public static void main(String[] args)
     {
+        double fahrenheit = 0;
+        double celsius = 0;
+        int userInput = 20;
+        
         Scanner scan = new Scanner(System.in);
+        while (userInput != 0)
+        {
+            System.out.println("Menu for converter");
+            System.out.println("1: Fahrenheit to Celsius");
+            System.out.println("2: Celsius to Fahrenheit");
+            System.out.println("0: To Exit Program");
+            System.out.println(" ");
+            userInput = scan.nextInt();
+            switch (userInput)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 0:
+                    System.out.println("Thank you for using.");
+                    break;
+                default:
+                    System.out.println("Invalid input");
+            }
 
+        }
+
+
+    }
+
+    public double getTemp(Scanner scan)
+    {
+        System.out.println("Please enter temperature.");
+        return scan.nextDouble();
     }
 }
