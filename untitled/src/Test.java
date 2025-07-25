1ef63d18eeaddb9f12990ca93797c277f60b851f
Tyler Rhoads <tyrhoads@gmail.com>
2025-07-24 22:44:17 -0400

    added

---
+++ untitled/src/Test.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test
+{
+
+    public static void main(String[] args) {
+        public String instructor;
+        public int class;
+        /** Insert missing code here **/
+        public bool equals (Object x) { ... }
+    }
+
+
+    }
+HashSet teachers;
+}
+
+public void main() {
+}
